7bf66af9321126efb40af682b3161ac915d589e0
Paul Franco <mail@paulfran.co>
2020-09-12 04:41:32 -0700

    added missing drawables

--- app/src/main/java/co/paulfran/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/co/paulfran/mvvmnewsapp/ui/NewsActivity.kt
@@ -2,12 +2,17 @@ package co.paulfran.mvvmnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import co.paulfran.mvvmnewsapp.R
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
 

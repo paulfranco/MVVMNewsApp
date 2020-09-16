abf883f85a2db1088a62e50fab12243b3d08a2c0
Paul Franco <mail@paulfran.co>
2020-09-15 21:04:23 -0700

    handle pagination

--- app/src/main/java/co/paulfran/mvvmnewsapp/models/NewsResponse.kt
+++ app/src/main/java/co/paulfran/mvvmnewsapp/models/NewsResponse.kt
@@ -3,7 +3,7 @@ package co.paulfran.mvvmnewsapp.models
 import co.paulfran.mvvmnewsapp.models.Article
 
 data class NewsResponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )

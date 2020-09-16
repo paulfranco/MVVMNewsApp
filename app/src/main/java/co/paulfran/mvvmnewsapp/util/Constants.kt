e2dfd63a7b7f7a88a42c9755719341d96dca5fe7
Paul Franco <mail@paulfran.co>
2020-09-16 03:06:10 -0700

    Update Constants.kt

--- app/src/main/java/co/paulfran/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/co/paulfran/mvvmnewsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package co.paulfran.mvvmnewsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "db386625a0a74754b151d252f3e570fe"
+        const val API_KEY = "YOUR API KEY"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
